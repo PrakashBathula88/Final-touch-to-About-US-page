ac5d7b0e737903442b2bd3d94cfb3d749918ad55
bathulapraka <bathulaprakash406@gmail.com>
2024-06-03 23:29:32 +0530

    Final touch to About US page

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+"use client"
+import Link from 'next/link';
+import React, { Fragment } from 'react'
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
 
 export default function Mypage() {
   return (
-    <div>My DynamicDetails</div>
+    <Fragment>
+      <h1>Developer Name</h1>
+      <ul>
+        {details.map((developer)=>(
+          <li key={developer.id}>
+             <Link href={`/aboutus/${developer.id}`}> {developer.name}</Link>
+          </li>  
+        ))}
+      </ul>
+    </Fragment>
   )
 }
